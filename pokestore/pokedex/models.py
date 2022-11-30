f26aace4b4abcd5e0817029c1a2cfca4f30fc677
Jordan <jbrockerrudow@gmail.com>
2022-11-30 09:11:12 -0600

    Initialize Pokemon Model

--- pokestore/pokedex/models.py
+++ pokestore/pokedex/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    def __init__(self, data):
+        self.abilities = data["abilities"]
+        self.base_experience = data["base_experience"]
+        self.forms = data["forms"]
+        self.game_indices = data["game_indices"]
+        self.held_items = data["held_items"]
+        self.height = data["height"]
+        self.held_items = data["held_items"]
+        self.id = data["id"]
+        self.is_default = data["is_default"]
