b408181cecffb52bb62d587fb154d9ee9341eedb
Jordan <jbrockerrudow@gmail.com>
2022-11-30 09:50:12 -0600

    Formatted with Black

--- pokestore/collection/models.py
+++ pokestore/collection/models.py
@@ -7,13 +7,17 @@ from versatileimagefield.fields import VersatileImageField, PPOIField
 
 class Tag(models.Model):
     value = models.TextField(max_length=100, unique=True)
+
     class Meta:
         ordering = ["value"]
-    
+
     def __str__(self):
         return self.value
 
+
 class Collection(models.Model):
-    name = models.
+    name = models.TextField(blank=True)
     pokemon = models.JSONField()
-    
+
+    def __str__(self):
+        return self.name
