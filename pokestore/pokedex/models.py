d980e57eedec33c0030ef455dadc92a3a8d26474
Jordan <jbrockerrudow@gmail.com>
2022-11-30 09:49:59 -0600

    Comment out fields

--- pokestore/pokedex/models.py
+++ pokestore/pokedex/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 
 class Abilities(models.Model):
-    self.pass
-
+    pass
 
 
 class Pokemon(models.Model):
-    self.abilities=
-    self.base_experience=
-    self.forms=
-    self.game_indices=
-    self.height=
-    self.held_items=
-    self.id=
-    self.is_default=
-    self.location_area_encounters=
-    self.moves=
-    self.name=
-    self.order=
-    self.past_types=
-    self.species=
-    self.sprites=
-    self.stats=
-    self.types=
-    self.weight=
+    #     self.abilities=
+    #     self.base_experience=
+    #     self.forms=
+    #     self.game_indices=
+    #     self.height=
+    #     self.held_items=
+    #     self.id=
+    #     self.is_default=
+    #     self.location_area_encounters=
+    #     self.moves=
+    #     self.name=
+    #     self.order=
+    #     self.past_types=
+    #     self.species=
+    #     self.sprites=
+    #     self.stats=
+    #     self.types=
+    #     self.weight=
+    pass
