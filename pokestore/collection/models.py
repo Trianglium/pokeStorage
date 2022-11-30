ad0c80a89ba144380641ac40d17005b2feea2fec
Jordan <jbrockerrudow@gmail.com>
2022-11-30 09:46:36 -0600

    Initialize Collection and Tag Models

--- pokestore/collection/models.py
+++ pokestore/collection/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from versatileimagefield.fields import VersatileImageField, PPOIField
 
-# Create your models here.
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100, unique=True)
+    class Meta:
+        ordering = ["value"]
+    
+    def __str__(self):
+        return self.value
+
+class Collection(models.Model):
+    name = models.
+    pokemon = models.JSONField()
+    
