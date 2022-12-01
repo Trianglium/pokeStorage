8f2672314f2858b38f11769de0135f9c36e007d5
Jordan <jbrockerrudow@gmail.com>
2022-12-01 12:35:23 -0600

    Add Base Exception for PokeAPI

---
+++ pokestore/pokeapi/exceptios.py
@@ -0,0 +1,6 @@
+
+class PokeApiException(Exception):
+    # Base Exception for all exceptions
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
