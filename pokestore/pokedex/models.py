c1f55d05a86b1ff5825f2f37922128b99537deee
Jordan <jbrockerrudow@gmail.com>
2022-12-01 12:38:48 -0600

    Add Veratile Image field

--- pokestore/pokedex/models.py
+++ pokestore/pokedex/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-
-class Abilities(models.Model):
-    pass
-
+from versatileimagefield.fields import VersatileImageField, PPOIField
 
 class Pokemon(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=200)
     #     self.abilities=
     #     self.base_experience=
     #     self.forms=
@@ -25,3 +27,16 @@ class Pokemon(models.Model):
     #     self.types=
     #     self.weight=
     pass
+    hero_image = VersatileImageField(
+        upload_to="hero_images", 
+        ppoi_field="ppoi", 
+        null=True, 
+        blank=True
+    )
+    # https://django-versatileimagefield.readthedocs.io/en/latest/model_integration.html#model-integration
+    
+    ppoi = PPOIField(null=True, blank=True)
+    # https://django-versatileimagefield.readthedocs.io/en/latest/specifying_ppoi.html#the-ppoifield
+
+    def __str__(self):
+        return f'{self.number}, {self.name}'
