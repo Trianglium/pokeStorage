1357c096579537970d0e25ae40dfc77f759949d6
Jordan <jbrockerrudow@gmail.com>
2022-12-01 12:31:40 -0600

    Remove redundant code

--- doc/data/data_util.py
+++ doc/data/data_util.py
@@ -6,18 +6,6 @@ f = open('abra.json')
 data = json.load(f)
 for i in data:
     print(i)
-    t = data[i]
-    if isinstance(t, bool) or isinstance(t, str):
-        fields[i] = t
-    else:
-        if isinstance(t, list):
-            print('\t', t)
-        
-        elif isinstance(t, dict):
-            print('\t', t.keys())
-
-        else: 
-            print(type(t), t)
 
 
 
