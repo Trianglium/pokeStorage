2ceb052aafd27d4126fc6d20b646d691a5b892f4
Jordan <jbrockerrudow@gmail.com>
2022-12-01 12:31:31 -0600

    Add GameIndice & Form Models

--- pokestore/pokeapi/models.py
+++ pokestore/pokeapi/models.py
@@ -1,5 +1,5 @@
 
-
+# abilities
 # {"ability":{"name":"synchronize","url":"https://pokeapi.co/api/v2/ability/28/"},"is_hidden":false,"slot":1}
 class Ability():
     def __init__(self, data):
@@ -7,3 +7,15 @@ class Ability():
         self.url = data["url"]
         self.is_hidden = data["is_hidden"]
         self.slot = data["slot"]
+
+
+# base_experience
+
+# forms
+class Form():
+    def __init__(self, data):
+        self.name = data["name"]
+# game_indices
+class GameIndice():
+    def __init__(self, data):
+        self.name = data["name"]
