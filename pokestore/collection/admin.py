ee6a4e14ff48d93d30f77b3ec54f19ebc28829c5
Jordan <jbrockerrudow@gmail.com>
2022-12-01 12:46:49 -0600

    Register Collections Models

--- pokestore/collection/admin.py
+++ pokestore/collection/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tag, Collection, TrainerProfile
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Collection)
+admin.site.register(TrainerProfile)
